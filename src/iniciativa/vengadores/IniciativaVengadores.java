d01c6ba0752bd3538ceaf531755a7ad790fe9a15
Barry Allen <35779615+BarryAllenTheflash@users.noreply.github.com>
2018-01-25 13:41:33 -0500

    Update IniciativaVengadores.java
    listo

--- src/iniciativa/vengadores/IniciativaVengadores.java
+++ src/iniciativa/vengadores/IniciativaVengadores.java
@@ -17,6 +17,7 @@ public class IniciativaVengadores {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Viva mexico");
     }
     
 }
